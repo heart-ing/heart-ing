156bce3c65e516563e4411d5ad5e175414f24deb
손민혁 <sonmh79@naver.com>
2024-01-28 10:23:55 +0900

    test: add test code on MessageService, MessageRepository

--- backend/src/test/java/com/chillin/hearting/db/repository/MessageRepositoryTest.java
+++ backend/src/test/java/com/chillin/hearting/db/repository/MessageRepositoryTest.java
@@ -1,13 +1,17 @@
 package com.chillin.hearting.db.repository;
 
+import com.chillin.hearting.api.data.HeartCountDTO;
+import com.chillin.hearting.api.service.AbstractTestData;
 import com.chillin.hearting.db.domain.Emoji;
 import com.chillin.hearting.db.domain.Heart;
 import com.chillin.hearting.db.domain.Message;
 import com.chillin.hearting.db.domain.User;
-import com.chillin.hearting.exception.NotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -16,91 +20,72 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @DataJpaTest
 //@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class MessageRepositoryTest {
+class MessageRepositoryTest extends AbstractTestData {
 
     @Autowired
     private MessageRepository messageRepository;
-    @Autowired
-    private UserRepository userRepository;
+
     @Autowired
     private HeartRepository heartRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Autowired
     private EmojiRepository emojiRepository;
 
-    private final Heart heart = Heart.builder().name("testHeart").imageUrl("testUrl").shortDescription("short").longDescription("long").type("type").acqCondition("acq").build();
-    private final User sender = User.builder().id("1").type("KAKAO").email("email1").nickname("test1").build();
-    private final User receiver = User.builder().id("2").type("KAKAO").email("email2").nickname("test2").build();
-    private final Emoji emoji = Emoji.builder().name("emoji").imageUrl("emojiUrl").build();
+    private User savedSender;
+    private User savedReceiver;
+    private Heart savedHeart;
+    private Emoji savedEmoji;
 
-    @Test
-    void messageRepositoryIsNotNull() {
-        assertThat(messageRepository).isNotNull();
+    @BeforeEach
+    void setup() {
+        savedSender = userRepository.save(createUser("sender"));
+        savedReceiver = userRepository.save(createUser("receiver"));
+        savedHeart = heartRepository.save(createHeart("name","DEFAULT"));
+        savedEmoji = emojiRepository.save(createEmoji("name"));
     }
 
     @Test
-    void successSendMessage() {
+    @DisplayName("메시지 저장")
+    void save() {
         // given
-        Message message = Message.builder().heart(heart).emoji(emoji).sender(sender).receiver(receiver).title("testTitle").content("message content").senderIp("127.0.0.1").build();
-
+        Message message = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .build();
         // when
-        heartRepository.save(heart);
-        userRepository.save(sender);
-        userRepository.save(receiver);
-        emojiRepository.save(emoji);
-
-        Message result = messageRepository.save(message);
+        Message savedMessage = messageRepository.save(message);
 
         // then
-        assertThat(result.getId()).isNotNull();
-        assertThat(result.getSender().getId()).isEqualTo(sender.getId());
-        assertThat(result.getReceiver().getId()).isEqualTo(receiver.getId());
-        assertThat(result.getHeart().getId()).isEqualTo(heart.getId());
-        assertThat(result.getEmoji().getId()).isEqualTo(emoji.getId());
+        assertThat(savedMessage.getId()).isEqualTo(message.getId());
     }
 
     @Test
-    void successUpdateUserMessageTotal() {
-        // given
-        Message message = Message.builder().heart(heart).emoji(emoji).sender(sender).receiver(receiver).title("testTitle").content("message content").senderIp("127.0.0.1").build();
-        
-        // when
-        heartRepository.save(heart);
-        userRepository.save(sender);
-        userRepository.save(receiver);
-        emojiRepository.save(emoji);
-
-        Message result = messageRepository.save(message);
-        User beforeUser = userRepository.findById(result.getReceiver().getId()).orElseThrow(NotFoundException::new);
-        long before = beforeUser.getMessageTotal();
-
-        beforeUser.updateMessageTotal();
-
-        userRepository.save(beforeUser);
-        User afterUser = userRepository.findById(result.getReceiver().getId()).orElseThrow(NotFoundException::new);
-        long after = afterUser.getMessageTotal();
-
-        // then
-        assertThat(result.getId()).isNotNull();
-        assertThat(after - before).isEqualTo(1);
-    }
-
-    @Test
+    @DisplayName("메시지 삭제")
     void successDeleteMessage() {
         // given
-        Message message = Message.builder().heart(heart).emoji(emoji).sender(sender).receiver(receiver).title("testTitle").content("message content").senderIp("127.0.0.1").build();
+        Message message = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .build();
 
         // when
-        heartRepository.save(heart);
-        userRepository.save(sender);
-        userRepository.save(receiver);
-        emojiRepository.save(emoji);
-
         Message messagePrev = messageRepository.save(message);
+
         // then
         assertThat(messagePrev.isActive()).isTrue();
 
         // when
         messagePrev.deleteMessage();
+        messageRepository.flush();
         Message messageAfter = messageRepository.save(messagePrev);
 
         // then
@@ -109,15 +94,18 @@ class MessageRepositoryTest {
     }
 
     @Test
+    @DisplayName("메시지 신고")
     void successReportMessage() {
         // given
-        Message message = Message.builder().heart(heart).emoji(emoji).sender(sender).receiver(receiver).title("testTitle").content("message content").senderIp("127.0.0.1").build();
+        Message message = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .build();
 
         // when
-        heartRepository.save(heart);
-        userRepository.save(sender);
-        userRepository.save(receiver);
-        emojiRepository.save(emoji);
         Message savedMessage = messageRepository.save(message);
 
         savedMessage.reportMessage();
@@ -128,29 +116,193 @@ class MessageRepositoryTest {
     }
 
     @Test
-    void successGetMessageDetail() {
+    @DisplayName("전체 메시지 조회")
+    void findAll() {
         // given
-        Message message1 = Message.builder().heart(heart).emoji(emoji).sender(sender).receiver(receiver).title("testTitle").content("message content").senderIp("127.0.0.1").build();
-        Message message2 = Message.builder().heart(heart).emoji(emoji).sender(sender).receiver(receiver).title("testTitle").content("message content").senderIp("127.0.0.1").build();
-        Message message3 = Message.builder().heart(heart).emoji(emoji).sender(sender).receiver(receiver).title("testTitle").content("message content").senderIp("127.0.0.1").build();
-        Message message4 = Message.builder().heart(heart).emoji(emoji).sender(sender).receiver(receiver).title("testTitle").content("message content").senderIp("127.0.0.1").build();
-        Message message5 = Message.builder().heart(heart).emoji(emoji).sender(sender).receiver(receiver).title("testTitle").content("message content").senderIp("127.0.0.1").build();
+        Message message1 = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .build();
+
+        Message message2 = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .build();
 
         // when
-        heartRepository.save(heart);
-        userRepository.save(sender);
-        userRepository.save(receiver);
-        emojiRepository.save(emoji);
         messageRepository.save(message1);
         messageRepository.save(message2);
-        messageRepository.save(message3);
-        messageRepository.save(message4);
-        messageRepository.save(message5);
 
         List<Message> messageList = messageRepository.findAll();
 
         // then
-        assertThat(messageList.size()).isEqualTo(5);
+        assertThat(messageList.size()).isEqualTo(2);
+    }
 
+    @Test
+    @DisplayName("수신자 ID로 활성화 상태의 메시지 조회")
+    void findByReceiverIdAndIsActiveTrue() {
+        // given
+        Message message = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .build();
+
+        messageRepository.save(message);
+
+        // when
+        List<Message> findMessageList = messageRepository.findByReceiverIdAndIsActiveTrue(savedReceiver.getId(), Sort.by(Sort.Direction.DESC, "createdDate"));
+
+        // then
+        assertThat(findMessageList).allMatch(msg -> msg.getReceiver().getId() == savedReceiver.getId() && msg.isActive() == true);
+    }
+
+    @Test
+    @DisplayName("수신자 ID와 송신자 IP로 찾기")
+    void findByReceiverIdAndSenderIp() {
+        // given
+        Message message = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .senderIp("senderIp")
+                .build();
+
+        messageRepository.save(message);
+
+        // when
+        List<Message> findMessageList = messageRepository.findByReceiverIdAndSenderIp(savedReceiver.getId(), message.getSenderIp());
+
+        // then
+        assertThat(findMessageList).allMatch(msg -> msg.getReceiver().getId() == savedReceiver.getId() && msg.getSenderIp() == msg.getSenderIp());
+
+    }
+
+    @Test
+    @DisplayName("송신자 Ip를 제외한 null 포함 메시지 수")
+    void countBySenderIpNotOrIsNull() {
+        // given
+        Message message = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .senderIp("senderIp")
+                .build();
+
+        Message adminMessage = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .senderIp("ADMIN")
+                .build();
+
+        messageRepository.save(message);
+
+        // when
+        long count = messageRepository.countBySenderIpNotOrIsNull(adminMessage.getSenderIp());
+
+        // then
+        assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("같은 사람에게 전송한 하트 별 메시지 수 중 최댓값")
+    void findMaxMessageCountToSameUser() {
+        // given
+        Message message1 = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .senderIp("senderIp")
+                .build();
+
+        Message message2 = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .senderIp("senderIp")
+                .build();
+
+        Message message3 = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .senderIp("senderIp")
+                .build();
+
+        messageRepository.save(message1);
+        messageRepository.save(message2);
+        messageRepository.save(message3);
+
+        // when
+        long result = messageRepository.findMaxMessageCountToSameUser(message1.getSender().getId(), savedHeart.getId());
+
+        // then
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("유저의 하트 별 메시지 전송 개수")
+    void findAllHeartSentCount() {
+        // given
+        Message message = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .senderIp("senderIp")
+                .build();
+
+        messageRepository.save(message);
+
+        // when
+        List<HeartCountDTO> findList = messageRepository.findAllHeartSentCount(message.getSender().getId());
+
+        // then
+        assertThat(findList).allMatch(dto -> (dto.getHeartId() == savedHeart.getId()) ? dto.getCurrentValue() == 1 : dto.getCurrentValue() == 0);
+    }
+
+    @Test
+    @DisplayName("유저의 하트 별 메시지 수신 개수")
+    void findAllHeartReceivedCount() {
+        // given
+        Message message = Message.builder()
+                .title("title")
+                .heart(savedHeart)
+                .emoji(savedEmoji)
+                .sender(savedSender)
+                .receiver(savedReceiver)
+                .senderIp("senderIp")
+                .build();
+
+        messageRepository.save(message);
+
+        // when
+        List<HeartCountDTO> findList = messageRepository.findAllHeartReceivedCount(message.getReceiver().getId());
+
+        // then
+        assertThat(findList).allMatch(dto -> (dto.getHeartId() == savedHeart.getId()) ? dto.getCurrentValue() == 1 : dto.getCurrentValue() == 0);
     }
 }
