2621699dfb49ae462f1b52eb9e8886f135a6dd21
손민혁 <sonmh79@naver.com>
2024-02-05 17:41:42 +0900

    refactor: replace HeartChecker init method position into inner class

--- backend/src/main/java/com/chillin/hearting/api/service/heartcheck/HeartChecker.java
+++ backend/src/main/java/com/chillin/hearting/api/service/heartcheck/HeartChecker.java
@@ -1,6 +1,8 @@
 package com.chillin.hearting.api.service.heartcheck;
 
 import com.chillin.hearting.api.data.HeartConditionData;
+import com.chillin.hearting.api.service.HeartService;
+import com.chillin.hearting.api.service.MessageService;
 
 import java.util.ArrayList;
 
@@ -9,8 +11,10 @@ public class HeartChecker {
     private HeartCheckStrategy heartCheckStrategy;
     private String userId;
 
-    public HeartChecker(String userId) {
+    public HeartChecker(String userId, long heartId, HeartService heartService, MessageService messageService) {
         this.userId = userId;
+        HeartCheckStrategyFactory strategyFactory = new HeartCheckStrategyFactory(heartService, messageService);
+        setHeartCheckStrategy(strategyFactory.createHeartCheckStrategy(heartId));
     }
 
     public void setHeartCheckStrategy(HeartCheckStrategy strategy) {
