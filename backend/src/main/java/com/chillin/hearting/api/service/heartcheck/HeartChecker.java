0003c1843e98ea0c903a7505966d20ac243afba1
손민혁 <sonmh79@naver.com>
2024-02-20 13:58:28 +0900

    test: refactor HeartChecker to be injected by Spring and add test code

--- backend/src/main/java/com/chillin/hearting/api/service/heartcheck/HeartChecker.java
+++ backend/src/main/java/com/chillin/hearting/api/service/heartcheck/HeartChecker.java
@@ -1,24 +1,24 @@
 package com.chillin.hearting.api.service.heartcheck;
 
 import com.chillin.hearting.api.data.HeartConditionData;
-import com.chillin.hearting.api.service.HeartService;
-import com.chillin.hearting.api.service.MessageService;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 
+@NoArgsConstructor
 public class HeartChecker {
 
-    private HeartCheckStrategy heartCheckStrategy;
     private String userId;
+    private HeartCheckStrategy heartCheckStrategy;
+    private HeartCheckStrategyFactory strategyFactory;
 
-    public HeartChecker(String userId, long heartId, HeartService heartService, MessageService messageService) {
-        this.userId = userId;
-        HeartCheckStrategyFactory strategyFactory = new HeartCheckStrategyFactory(heartService, messageService);
-        setHeartCheckStrategy(strategyFactory.createHeartCheckStrategy(heartId));
+    public HeartChecker(HeartCheckStrategyFactory strategyFactory) {
+        this.strategyFactory = strategyFactory;
     }
 
-    public void setHeartCheckStrategy(HeartCheckStrategy strategy) {
-        this.heartCheckStrategy = strategy;
+    public void init(String userId, long heartId) {
+        this.userId = userId;
+        this.heartCheckStrategy = strategyFactory.createHeartCheckStrategy(heartId);
     }
 
     public boolean isAcquirable() {
